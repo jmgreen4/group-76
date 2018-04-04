8907868eb042a83976e83db396fa1f733e1bbbda
baglets <samuelmt@vt.edu>
2018-04-04 15:49:31 -0400

    Updated with date and description (to test pushing and pulling)

--- Project 5 Group 76/src/project/ProjectRunner.java
+++ Project 5 Group 76/src/project/ProjectRunner.java
@@ -1,16 +1,16 @@
 // Virginia Tech Honor Code Pledge:
 //
 // As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, 
+// I will not lie, cheat, or steal,
 // nor will I accept the actions of those who
 // do.
 // -- Samuel Tyson (samuelmt)
 
 /**
- * This class 
+ * This class is the project runner. It runs the project (Duh).
  *
  * @author <Samuel Tyson> <samuelmt>
- * @version 2018.MM.DD
+ * @version 2018.04.04
  */
 package project;
 
