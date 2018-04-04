61baae58c4bc52601d9656d720bc91b2dde5716f
baglets <samuelmt@vt.edu>
2018-04-04 14:23:54 -0400

    Initial Commit
    Signed-off-by: baglets <samuelmt@vt.edu>

---
+++ Project 5 Group 76/src/project/ProjectRunner.java
@@ -0,0 +1,31 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, 
+// nor will I accept the actions of those who
+// do.
+// -- Samuel Tyson (samuelmt)
+
+/**
+ * This class 
+ *
+ * @author <Samuel Tyson> <samuelmt>
+ * @version 2018.MM.DD
+ */
+package project;
+
+/**
+ * @author samue
+ *
+ */
+public class ProjectRunner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
