c1342ebc58500571c82c284933a62c5d866d4d0c
baglets <samuelmt@vt.edu>
2018-04-04 15:53:50 -0400

    Added the 3 enums

---
+++ Project 5 Group 76/src/project/RegionEnum.java
@@ -0,0 +1,23 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, 
+// nor will I accept the actions of those who
+// do.
+// -- Samuel Tyson (samuelmt)
+
+/**
+ * This class 
+ *
+ * @author <Samuel Tyson> <samuelmt>
+ * @version 2018.MM.DD
+ */
+package project;
+
+/**
+ * @author samue
+ *
+ */
+public enum RegionEnum {
+    NORTHEAST, SOUTHEAST, REST, OUTSIDE
+}
