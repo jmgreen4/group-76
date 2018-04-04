99a91d87bacd290fef66f1fabcc4fd72bc0226ac
jmgreen4 <36116940+jmgreen4@users.noreply.github.com>
2018-04-04 16:25:26 -0400

    Create DataGetter.java

---
+++ Project 5 Group 76/src/project/DataGetter.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package prj5;
+
+import java.util.Scanner;
+
+/**
+ * @author jmgreen4
+ *
+ */
+public class DataGetter {
+    
+    private Scanner songsScanner;
+    private Scanner respScanner;
+    
+    public DataGetter(Scanner songsScanner, Scanner respScanner)
+    {
+        this.songsScanner = songsScanner;
+        this.respScanner = respScanner;
+    }
+    
+    public SongList<Song> makeList() {
+        
+    }
+    
+    private boolean inputSongs() {
+    
+    }
+    
+    private boolean inputResponses() {
+    
+    }
+}
+
